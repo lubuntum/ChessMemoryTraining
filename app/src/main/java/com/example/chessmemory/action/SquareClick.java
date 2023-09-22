7aea5ec98e408afe266a1e467be7121f49910048
lubuntum <lubuntum@gmail.com>
2023-09-22 12:35:01 +0700

    Подправил событие клика для доски с запоминанием предыдущей позиции для возвращения цвета

--- app/src/main/java/com/example/chessmemory/action/SquareClick.java
+++ app/src/main/java/com/example/chessmemory/action/SquareClick.java
@@ -1,30 +1,42 @@
 package com.example.chessmemory.action;
 
 import android.content.Context;
+import android.content.res.ColorStateList;
 import android.view.View;
 import android.widget.ImageView;
 
 import com.example.chessmemory.ChessGameFragment;
 import com.example.chessmemory.R;
+import com.example.chessmemory.gamesettings.GameBoardInfo;
 
 import java.util.List;
 
 public class SquareClick {
+    public int clickColor;
+    public int whiteColor;
+    public int blackColor;
     public Context context;
     public View previousSquare = null;
     public View.OnClickListener squareListener;
     public SquareClick(Context context){
         this.context = context;
+        clickColor = context.getResources().getColor(R.color.highlighted);
+        whiteColor = context.getResources().getColor(com.beardedhen.androidbootstrap.R.color.bootstrap_gray_lighter);
+        blackColor = context.getResources().getColor(com.beardedhen.androidbootstrap.R.color.bootstrap_gray_light);
         this.squareListener = view -> {
             if(previousSquare != null){
-                if(previousSquare.getTag() == ChessGameFragment.ChessColor.WHITE)
+                if(previousSquare.getTag() == GameBoardInfo.ChessColor.WHITE)
                     previousSquare
-                            .setBackgroundColor(context.getResources().getColor(com.beardedhen.androidbootstrap.R.color.bootstrap_gray_lighter));
-                else previousSquare.setBackgroundColor(context.getResources().getColor(com.beardedhen.androidbootstrap.R.color.bootstrap_gray_light));
-
-                view.setBackgroundColor(context.getResources().getColor(R.color.highlighted));
+                            .setBackgroundColor(whiteColor);
+                else previousSquare.setBackgroundColor(blackColor);
+                if(view.equals(previousSquare)) {
+                    previousSquare = null;
+                    return;
+                }
+                view.setBackgroundColor(clickColor);
             }
-            view.setBackgroundColor(context.getResources().getColor(R.color.highlighted));
+
+            view.setBackgroundColor(clickColor);
             previousSquare =  view;
             //view.setPadding(10,10,10,10);
         };
